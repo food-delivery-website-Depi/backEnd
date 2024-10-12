f9b15205a7ba209fff47bfee7ed278af6a1755c4
Som3a <som3a11202@gmail.com>
2024-10-12 08:53:55 +0300

    fuck

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import { placeOrder, verifyOrder, userOrders } from "../controllers/orderController"
+import { placeOrder, verifyOrder, userOrders } from "../controllers/orderController.js"
 
 const orderRouter = express.Router();
 
